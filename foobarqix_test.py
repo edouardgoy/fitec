f0af74209179a8503a2df0d014b32714eab49f2f
edouardgoy <33248788+edouardgoy@users.noreply.github.com>
2017-10-31 14:21:58 +0100

    4 TEST 6 + 9

--- foobarqix_test.py
+++ foobarqix_test.py
@@ -19,6 +19,8 @@ def foobarqix(number):
 class FooBarQixTestCase(unittest.TestCase):
     def test_return_Foo_when_number_is_divisible_by_3(self):
         self.assertEqual(foobarqix(3), "Foo")
+        self.assertEqual(foobarqix(6), "Foo")
+        self.assertEqual(foobarqix(9), "Foo")
 
     def test_return_Bar_when_number_is_divisible_by_5(self):
         self.assertEqual(foobarqix(5), "Bar")
