e69adaf215925dd75f30bcc3c201db578d3edd9f
edouardgoy <33248788+edouardgoy@users.noreply.github.com>
2017-10-31 11:51:26 +0100

    TEST 2 DIVISIBILITE PAR 5

--- foobarqix_test.py
+++ foobarqix_test.py
@@ -2,12 +2,18 @@ import unittest
 
 
 def foobarqix(number):
-    return  "Foo"
+    if number % 3 == 0:
+        return  "Foo"
+    else:
+        return "Bar"
 
 
 class FooBarQixTestCase(unittest.TestCase):
     def test_return_Foo_when_number_is_divisible_by_3(self):
         self.assertEqual(foobarqix(3), "Foo")
+
+    def test_return_Bar_when_number_is_divisible_by_5(self):
+        self.assertEqual(foobarqix(5), "Bar")
 
 
 unittest.main()
