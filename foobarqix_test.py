959a08be5b0cf12ea3cebbbd22ac08954e25d72b
edouardgoy <33248788+edouardgoy@users.noreply.github.com>
2017-10-31 15:01:34 +0100

    LIEN TEST FOOBARQIX

--- foobarqix_test.py
+++ foobarqix_test.py
@@ -1,23 +1,5 @@
 import unittest
-
-
-def is_divisible(number, divisor):
-    return number % divisor == 0
-
-
-def foobarqix(number):
-    result = ""
-    if is_divisible(number, 3):
-        result += "Foo"
-    if is_divisible(number, 5):
-        result += "Bar"
-    if is_divisible(number, 7):
-        result += "Qix"
-    if result != "":
-        return result
-    else:
-        return str(number)
-
+from foobarqix import foobarqix
 
 class FooBarQixTestCase(unittest.TestCase):
     def test_return_Foo_when_number_is_divisible_by_3(self):
